5d1dd9dce24d55793be0c3154111b4bef7682e69
Guilherme Henrique Luchesi <gui.olhenrique@gmail.com>
2018-06-16 14:03:12 -0300

    Troca print de the_field por get_meta

--- themes/votasp/realizadores.php
+++ themes/votasp/realizadores.php
@@ -1,5 +1,5 @@
 <li class="colaboradores-item">
     <?php the_post_thumbnail( [100, 100] ); ?>
     <p><?php the_title(); ?></p>
-    <span><?php the_field( 'realizador_task' ); ?></span>
+    <span><?php echo get_post_meta( get_the_ID(), '_colaborou_com', true ); ?></span>
 </li>
