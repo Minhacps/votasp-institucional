7a07c67e4e2a16763cfb34a659fc2d8ed611af68
Guilherme Henrique Luchesi <gui.olhenrique@gmail.com>
2018-06-18 23:11:23 -0300

    Atualiza hamburguer menu

--- themes/votasp/header.php
+++ themes/votasp/header.php
@@ -35,7 +35,17 @@
             <?php odin_the_custom_logo(); ?>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 22.67 22.82">
+                    <defs>
+                        <style>.cls-1{fill:#fff;}</style>
+                    </defs>
+                    <title>Menu Mobile</title>
+                    <g>
+                        <path class="cls-1" d="M20.94,0H1.7A1.71,1.71,0,0,0,0,1.7H0A1.72,1.72,0,0,0,1.7,3.4H21a1.72,1.72,0,0,0,1.7-1.7h0A1.74,1.74,0,0,0,20.94,0Z"/>
+                        <path class="cls-1" d="M20.94,19.42H1.7A1.72,1.72,0,0,0,0,21.12H0a1.71,1.71,0,0,0,1.7,1.7H21a1.72,1.72,0,0,0,1.7-1.7h0A1.75,1.75,0,0,0,20.94,19.42Z"/>
+                        <path class="cls-1" d="M20.94,9.73H1.7A1.71,1.71,0,0,0,0,11.43H0a1.71,1.71,0,0,0,1.7,1.69H21a1.72,1.72,0,0,0,1.7-1.69h0A1.74,1.74,0,0,0,20.94,9.73Z"/>
+                    </g>
+                </svg>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNav">
